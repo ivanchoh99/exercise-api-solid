cc72cb06d5bf7c11786246acc342eceb5405dafa
Iván Hernán Carvajal Acevedo <ivanchoh99@gmail.com>
2024-08-13 19:19:46 -0500

    update: entity's in the project to ORM

--- src/main/java/org/examplesolid/domain/model/entity/CharacterEntity.java
+++ src/main/java/org/examplesolid/domain/model/entity/CharacterEntity.java
@@ -9,17 +9,35 @@ import lombok.Setter;
 import java.util.List;
 import java.util.UUID;
 
-@Entity(name = "character_tv")
-@AllArgsConstructor
-@NoArgsConstructor
+@Entity
+@Table(name = "character_tv")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CharacterEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID uuid;
     @Column(nullable = false)
     private String name;
-    @OneToMany
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "CHARACTER_EPISODE",
+            joinColumns = @JoinColumn(name = "CHARACTER_UUID", referencedColumnName = "uuid"),
+            inverseJoinColumns = @JoinColumn(name = "EPISODE_UUID", referencedColumnName = "uuid")
+    )
     private List<EpisodeEntity> episodes;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "CHARACTER_FUNFACTS",
+            joinColumns = @JoinColumn(name = "CHARACTER_UUID", referencedColumnName = "uuid"),
+            inverseJoinColumns = @JoinColumn(name = "FUN_FACTS_UUID", referencedColumnName = "uuid")
+    )
+    private List<FunFacts> funFacts;
+
+    public CharacterEntity(String name, List<String> episodes) {
+        this.name = name;
+        this.episodes = episodes.stream().map(EpisodeEntity::new).toList();
+    }
 }
--- src/main/java/org/examplesolid/domain/model/entity/EpisodeEntity.java
+++ src/main/java/org/examplesolid/domain/model/entity/EpisodeEntity.java
@@ -1,14 +1,23 @@
 package org.examplesolid.domain.model.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
-@Entity(name ="episode")
+@Entity
+@Table(name = "episodes")
+@Getter
+@NoArgsConstructor
 public class EpisodeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID uuid;
-    @Column(unique = true)
-    private String url;
+    @Column(unique = true, name = "number")
+    private Integer number;
+
+    public EpisodeEntity(String number){
+        this.number = Integer.getInteger(number);
+    }
 }
