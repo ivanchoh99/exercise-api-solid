250baab944401ad451da924a36fcd87fc87ca576
Iván Hernán Carvajal Acevedo <ivanchoh99@gmail.com>
2024-08-19 17:00:01 -0500

    update: controller of api

--- src/main/java/org/examplesolid/infrastructure/rest/controller/CharactersController.java
+++ src/main/java/org/examplesolid/infrastructure/rest/controller/CharactersController.java
@@ -1,25 +1,29 @@
 package org.examplesolid.infrastructure.rest.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.examplesolid.domain.model.dto.Character;
 import org.examplesolid.domain.model.dto.CharacterSimple;
 import org.examplesolid.domain.model.entity.CharacterEntity;
-import org.examplesolid.domain.port.service.ICharacterService;
+import org.examplesolid.domain.port.service.ICharacter;
+import org.examplesolid.infrastructure.rest.dto.request.NewFunFactRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.naming.NameNotFoundException;
 import java.util.List;
 
+@Tag(name = "Character", description = "Using Rick and Morty API practice some exercise and endpoints")
 @RestController
 @RequestMapping("/api/solid")
 public class CharactersController {
 
-    private final ICharacterService service;
+    private final ICharacter service;
 
     @Autowired
-    public CharactersController(ICharacterService service) {
+    public CharactersController(ICharacter service) {
         this.service = service;
     }
 
@@ -30,24 +34,19 @@ public class CharactersController {
 
     @GetMapping("/charaters-db-pag")
     public ResponseEntity<List<Character>> gerCharactersDB(
-            @RequestParam(name = "page",defaultValue = "0") Integer page,
-            @RequestParam(name = "size",defaultValue = "3")Integer size) {
+            @RequestParam(name = "page", defaultValue = "0", required = false) Integer page,
+            @RequestParam(name = "size", defaultValue = "100", required = false) Integer size) {
         Pageable pageable = PageRequest.of(page, size);
         return ResponseEntity.ok(service.getAllCharactersFromDBInPage(pageable));
     }
 
-    @GetMapping("/charaters-db")
+    @GetMapping("/characters-db")
     public ResponseEntity<List<CharacterEntity>> gerCharactersDB() {
         return ResponseEntity.ok(service.getAllCharactersFromDB());
     }
 
-    @GetMapping("/character/{name}")
-    public ResponseEntity<Character> getCharacterByName(@PathVariable(name = "name") String name){
-        return ResponseEntity.ok(service.getCharacterFromDB(name));
-    }
-
     @PostMapping("/persist-characters")
-    public ResponseEntity<Void> persistCharactersFromApi(){
+    public ResponseEntity<Void> persistCharactersFromApi() {
         service.saveCharactersFromApi();
         return ResponseEntity.ok().build();
     }
