d240646638829fe1ee94be293d809ca29a5b5c92
Iván Hernán Carvajal Acevedo <ivanchoh99@gmail.com>
2024-08-22 15:02:38 -0500

    update & add: change to #development of PR comments about Sensitive Data config

--- src/main/java/org/examplesolid/application/config/ApiDocConfig.java
+++ src/main/java/org/examplesolid/application/config/ApiDocConfig.java
@@ -16,14 +16,18 @@ public class ApiDocConfig {
 
     @Value("${api.doc.dev-url}")
     private String devUrl;
+    @Value("${api.owner}")
+    private String owner;
+    @Value("${api.email-support}")
+    private String emailSupport;
 
     @Bean
-    public OpenAPI myOpenAPI() {
+    public OpenAPI configToUiDocumentation() {
         Server devServer = new Server();
         devServer.setUrl(devUrl);
         Contact contact = new Contact();
-        contact.setEmail("ivanchoh99@gmail.com");
-        contact.setName("Ivan H Carvajal Acevedo");
+        contact.setEmail(emailSupport);
+        contact.setName(owner);
         License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
         Info info = new Info().title("Traninig example API").version("0.5").contact(contact).description("This API is a exercise mentoring by Juan Manuel to improve and get better tech concepts").license(mitLicense);
         return new OpenAPI().info(info).servers(List.of(devServer));
