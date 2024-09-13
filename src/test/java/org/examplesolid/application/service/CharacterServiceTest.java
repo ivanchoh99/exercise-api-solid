ae8e9ad299294252d0eca19d884caa3eb9d9d35b
Iván Hernán Carvajal Acevedo <ivanchoh99@gmail.com>
2024-09-12 22:31:28 -0500

    update test

--- src/test/java/org/examplesolid/application/service/CharacterServiceTest.java
+++ src/test/java/org/examplesolid/application/service/CharacterServiceTest.java
@@ -2,18 +2,20 @@ package org.examplesolid.application.service;
 
 import org.examplesolid.application.model.api.ApiResponse;
 import org.examplesolid.application.model.api.CharacterApi;
+import org.examplesolid.application.model.api.Location;
 import org.examplesolid.application.model.response.CharacterResponse;
 import org.examplesolid.infrastructure.api.RickAndMortyAPIClient;
 import org.examplesolid.infrastructure.db.entity.CharacterEntity;
 import org.examplesolid.infrastructure.db.repository.CharacterRepository;
 import org.examplesolid.infrastructure.mapper.CharacterMapper;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.MockitoAnnotations;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.record.RecordModule;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
@@ -23,81 +25,92 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CharacterServiceTest {
 
-    private static RickAndMortyAPIClient api;
-    private static CharacterRepository repository;
-    private static CharacterService service;
+    @Mock
+    private RickAndMortyAPIClient api;
+    @Mock
+    private CharacterRepository repository;
+    @Spy
+    private CharacterMapper mapper;
+    @InjectMocks
+    private CharacterService service;
 
-
-    @BeforeAll
-    static void setUpTestClass() throws Exception {
-        MockitoAnnotations.openMocks(CharacterServiceTest.class).close();
-        api = mock(RickAndMortyAPIClient.class);
-        repository = mock(CharacterRepository.class);
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.registerModule(new RecordModule());
-        CharacterMapper mapper = new CharacterMapper();
-        service = new CharacterService(api, repository, mapper);
-    }
+    //* Arrange Class Test
+    private final UUID uuid = UUID.randomUUID();
+    private final String nameCharacter1 = "Rick Sanchez";
+    private final String nameCharacter2 = "Alberto";
 
     @Test
     void givenAFunFactAndNameCharacter_whenAddTheFunFactToTheEntity_thenReturnTheCharacterModify() throws NameNotFoundException {
         //* Arrange
-        UUID uuid = UUID.randomUUID();
-        String nameCharacter = "Rick Sanchez";
-        String fact1 = "Abuelo";
-        String fact2 = "Cientifico";
-        CharacterEntity entity = new CharacterEntity();
-        entity.setName(nameCharacter);
-        entity.setFacts(fact1);
+        CharacterEntity find = new CharacterEntity();
         CharacterEntity persistedEntity = new CharacterEntity();
-        persistedEntity.setUuid(UUID.randomUUID());
-        persistedEntity.setName(nameCharacter);
-        persistedEntity.setFacts(fact1);
-        when(repository.findByName(nameCharacter)).thenReturn(entity);
-        when(repository.save(entity)).thenReturn(persistedEntity);
+        find.setUuid(uuid);
+        find.setName(nameCharacter1);
+        String fact1 = "Abuelo";
+        find.setFacts(fact1);
+        persistedEntity.setUuid(uuid);
+        persistedEntity.setName(nameCharacter1);
+        String fact2 = "Cientifico";
+        persistedEntity.setFacts(fact1 + "-" + fact2);
+        when(repository.findByName(nameCharacter1)).thenReturn(find);
+        when(repository.save(any(CharacterEntity.class))).thenReturn(persistedEntity);
         //* Act
-        CharacterResponse result = service.addFact(nameCharacter, fact2);
+        CharacterResponse result = service.addFact(nameCharacter1, fact2);
         //* Assert
-        assertThat(result.getFacts())
-                .as("Add a fun fact to character correctly")
+        assertThat(result.getFacts()).as("Add a fun fact to character correctly")
                 .withFailMessage("The fun facts character don't be modify correctly")
                 .contains(fact1, fact2);
     }
 
     @Test
-    void consumingTheApiOrGetDataFromDb_whenGetCharacters_thenSortByName() {
+    void getCharacters_whenDbIsEmpty_thenConsumeApiAndReturnCharactersSorted() {
         //* Arrange
-        Pageable pageable = PageRequest.of(0, 100);
+        Location location = new Location("locationName", "url");
         CharacterApi characterApi1 = new CharacterApi();
-        characterApi1.setName("Alberto");
+        characterApi1.setName(nameCharacter2);
+        characterApi1.setEpisode(List.of("https://rickandmortyapi.com/api/episode/1", "https://rickandmortyapi.com/api/episode/2"));
+        characterApi1.setLocation(location);
+        characterApi1.setOrigin(location);
         CharacterApi characterApi2 = new CharacterApi();
-        characterApi2.setName("Zuñiga");
+        characterApi2.setName(nameCharacter1);
+        characterApi1.setEpisode(List.of("https://rickandmortyapi.com/api/episode/1", "https://rickandmortyapi.com/api/episode/2"));
+        characterApi2.setLocation(location);
+        characterApi2.setOrigin(location);
         ApiResponse response = new ApiResponse(null, List.of(characterApi1, characterApi2));
+        when(repository.findAll(any(Pageable.class))).thenReturn(Page.empty());
         when(api.getRickAndMortyCharacters()).thenReturn(response);
         //* Act
-        List<CharacterResponse> characterSimples = service.getSortedCharacters(pageable);
+        List<CharacterResponse> characters = service.getSortedCharacters(Page.empty().getPageable());
         //* Assert
-        assertThat(characterSimples)
-                .as("Characters Simples are order alphabetically")
-                .withFailMessage("The Characters Simples are not order alphabetically")
+        assertThat(characters)
+                .as("Characters are order alphabetically")
+                .withFailMessage("The Characters are not order alphabetically")
                 .isSortedAccordingTo(Comparator.comparing(CharacterResponse::getName));
     }
 
     @Test
-    void consumingTheApi_whenGetEmptyCharacterResponseFromApi_thenReturnEmptyList() {
+    void getCharacters_whenDbIsNotEmpty_thenFindInDbAndReturnCharactersSorted() {
         //* Arrange
-        Pageable pageable = PageRequest.of(0, 100);
-        ApiResponse response = new ApiResponse(null, List.of());
-        when(api.getRickAndMortyCharacters()).thenReturn(response);
+        Pageable pageable = PageRequest.of(0, 50);
+        CharacterEntity character1 = new CharacterEntity();
+        character1.setName(nameCharacter1);
+        CharacterEntity character2 = new CharacterEntity();
+        character2.setName(nameCharacter2);
+        Page<CharacterEntity> entities = new PageImpl<>(List.of(character1, character2));
+        when(repository.findAll(any(Pageable.class))).thenReturn(entities);
         //* Act
-        List<CharacterResponse> characterSimples = service.getSortedCharacters(pageable);
+        List<CharacterResponse> characters = service.getSortedCharacters(pageable);
         //* Assert
-        assertThat(characterSimples).as("Validate that getSortedCharacters can return empty list").withFailMessage("The Characters Simples list is not empty or aren't instance").isEmpty();
+        assertThat(characters)
+                .as("Characters are order alphabetically")
+                .withFailMessage("The Characters are not order alphabetically")
+                .isSortedAccordingTo(Comparator.comparing(CharacterResponse::getName));
+
     }
 }
--- src/test/java/org/examplesolid/infrastructure/endpoints/controller/CharactersControllerTest.java
+++ src/test/java/org/examplesolid/infrastructure/endpoints/controller/CharactersControllerTest.java
@@ -1,5 +1,96 @@
 package org.examplesolid.infrastructure.endpoints.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.examplesolid.application.model.response.CharacterResponse;
+import org.examplesolid.application.service.CharacterService;
+import org.examplesolid.infrastructure.endpoints.dto.request.FactRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import javax.naming.NameNotFoundException;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CharactersController.class)
 class CharactersControllerTest {
 
+    private static final String BASE_URL = "/api/characters";
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private CharacterService service;
+
+    @Test
+    void getCharacters() throws Exception {
+        //* Arrange
+        CharacterResponse character1 = new CharacterResponse();
+        CharacterResponse character2 = new CharacterResponse();
+        character1.setName("Rick Sanchez");
+        character2.setName("Morty Smith");
+        List<CharacterResponse> characters = List.of(character1, character2);
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("page", "0");
+        params.add("size", "100");
+        when(service.getSortedCharacters(any(Pageable.class))).thenReturn(characters);
+        //*Act & Assert
+        mockMvc.perform(get(BASE_URL)
+                        .params(params))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Rick Sanchez"))  // Verifica el nombre del primer personaje
+                .andExpect(jsonPath("$[1].name").value("Morty Smith"));
+    }
+
+    @Test
+    void postAddFact() throws Exception {
+        //* Arrange
+        String nameCharacter = "Rick";
+        String newFact = "newFact";
+        FactRequest factRequest = new FactRequest(newFact, nameCharacter);
+        CharacterResponse characterResponse = new CharacterResponse();
+        characterResponse.setName(nameCharacter);
+        characterResponse.setFacts(List.of("firstFact", factRequest.funFact()));
+        String factRequestJson = objectMapper.writeValueAsString(factRequest);
+        when(service.addFact(nameCharacter, newFact)).thenReturn(characterResponse);
+        //* Act & Assert
+        mockMvc.perform(post(BASE_URL.concat("/fact"))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(factRequestJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(nameCharacter))
+                .andExpect(jsonPath("$.facts[0]").value("firstFact"))
+                .andExpect(jsonPath("$.facts[1]").value(newFact));
+    }
+
+    @Test
+    void postAddFact_AndNotFoundCharacter() throws Exception {
+        //* Arrange
+        String nameCharacter = "Rick";
+        String newFact = "newFact";
+        FactRequest factRequest = new FactRequest(newFact, nameCharacter);
+        String factRequestJson = objectMapper.writeValueAsString(factRequest);
+        when(service.addFact(nameCharacter, newFact)).thenThrow(new NameNotFoundException("character with name " + nameCharacter + " not found"));
+        //* Act & Assert
+        mockMvc.perform(post(BASE_URL.concat("/fact"))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(factRequestJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("character with name " + nameCharacter + " not found"));
+    }
 }
