25371602d3de71ac643ba0cc1d7665097e016351
Iván Hernán Carvajal Acevedo <ivanchoh99@gmail.com>
2024-09-12 22:34:32 -0500

    add modelMapper config to future use

---
+++ src/main/java/org/examplesolid/infrastructure/mapper/MapperConfig.java
@@ -0,0 +1,16 @@
+package org.examplesolid.infrastructure.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.record.RecordModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.registerModule(new RecordModule());
+        return modelMapper;
+    }
+}
