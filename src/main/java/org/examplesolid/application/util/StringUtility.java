f55bd8f62cd584d7cc52ff6e19eceed880437fd8
Iván Hernán Carvajal Acevedo <ivanchoh99@gmail.com>
2024-09-14 10:29:57 -0500

    change StringUtility with varargs instead of overloading

--- src/main/java/org/examplesolid/application/util/StringUtility.java
+++ src/main/java/org/examplesolid/application/util/StringUtility.java
@@ -7,32 +7,17 @@ import java.util.stream.Collectors;
 
 public class StringUtility {
 
-
-    public static List<String> stringToList(String string) {
+    public static List<String> stringToList(String string, String... delimiter) {
         if (string == null || string.isBlank() || string.isEmpty()) return new ArrayList<>();
-        return Arrays.stream(string.split("-")).collect(Collectors.toList());
+        return Arrays.stream(string.split(delimiter.length >= 1 ? delimiter[0] : "-")).collect(Collectors.toList());
     }
 
-    public static List<String> stringToList(String string, String delimiter) {
-        if (string == null || string.isBlank() || string.isEmpty()) return new ArrayList<>();
-        return Arrays.stream(string.split(delimiter)).collect(Collectors.toList());
-    }
-
-    public static String listToString(List<String> listStrings) {
+    public static String listToString(List<String> listStrings, String... delimiter) {
         if (listStrings == null || listStrings.isEmpty()) return "";
-        return String.join("-", listStrings);
+        return String.join(delimiter.length >= 1 ? delimiter[0] : "-", listStrings);
     }
 
-    public static String listToString(List<String> listStrings, String delimiter) {
-        if (listStrings == null || listStrings.isEmpty()) return "";
-        return String.join(delimiter, listStrings);
-    }
-
-    public static String joinStrings(String baseString, String attachString) {
-        return baseString.concat("-").concat(attachString);
-    }
-
-    public static String joinStrings(String baseString, String attachString, String delimiter) {
-        return baseString.concat(delimiter).concat(attachString);
+    public static String joinStrings(String baseString, String attachString, String... delimiter) {
+        return baseString.concat(delimiter.length == 1 ? delimiter[0] : "-").concat(attachString);
     }
 }
